a18370ca19c503cd16f1bb3829f3cc67a4ed60ec
Ashab <ashabuddin54321@gmail.com>
2021-11-28 11:20:21 +0600

    adminDelete

--- controllers/userController.js
+++ controllers/userController.js
@@ -154,11 +154,13 @@ exports.updateProfile = catchAsyncErrors(async(req, res, next)=>{
     name: req.body.name,
     email:req.body.email
   }
+
   const user = await User.findByIdAndUpdate(req.user.id, newUserData,{
     new: true,
     runValidators:true,
     useFindAndModify: false
   })
+
   res.status(200).json({
     success: true,
   })
@@ -184,6 +186,44 @@ exports.getSingleUser = catchAsyncErrors(async(req, res, next)=>{
      user
    })
  })
+
+ 
+//Update User Role -- Admin
+exports.updateUserRole = catchAsyncErrors(async(req, res, next)=>{
+  
+  const newUserData ={
+    name: req.body.name,
+    email:req.body.email,
+    role: req.body.role
+  }
+
+  const user = await User.findByIdAndUpdate(req.params.id, newUserData,{
+    new: true,
+    runValidators:true,
+    useFindAndModify: false
+  })
+  
+  res.status(200).json({
+    success: true,
+  })
+})
+ 
+//Delete User -- Admin
+exports.deleteUser = catchAsyncErrors(async(req, res, next)=>{
+
+ const user = await User.findById(req.params.id)  
+ 
+ if (!user) {
+   return next( new ErrorHandler(`User does not exist with Id: ${req.params.id}`,400))
+ }
+ await user.remove()
+
+  res.status(200).json({
+    success: true,
+    message: "User Deleted Successfully",
+  })
+})
+
 
 
 
