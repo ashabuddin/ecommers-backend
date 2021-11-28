e0f3efb01e35943cc4fc684c18a1cc97638c8be3
Ashab <ashabuddin54321@gmail.com>
2021-11-28 21:41:14 +0600

    order

--- app.js
+++ app.js
@@ -13,11 +13,13 @@ app.use(cookieParser())
 // Route Imports
 const product = require("./routes/productRoutes");
 const user = require("./routes/userRoute");
+const order = require("./routes/orderRoute");
 
 
 // application routes
 app.use("/api/v1", product);
 app.use("/api/v1", user);
+app.use("/api/v1", order);
 
 // Middleware for Errors
 app.use(errorMiddleware);
