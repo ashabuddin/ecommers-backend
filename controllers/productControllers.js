fe673daf7a712b5279e6d72561f6a5a681843ba6
Ashab <ashabuddin54321@gmail.com>
2021-12-13 11:52:01 +0600

    client

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -39,8 +39,7 @@ exports.getProductDetails = catchAsyncErrors( async (req,res,next) => {
   }
   res.status(200).json({
     success: true,
-    product,
-    productsCount
+    product, 
   })
 
 })
